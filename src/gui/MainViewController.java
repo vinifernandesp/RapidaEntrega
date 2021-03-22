4a1c8e21b978ede55a711746cb37f1cbcbdfa659
Vinícius Fernandes <vinifernandesp@outlook.com.br>
2021-03-22 18:44:31 -0300

    Sobre view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -38,7 +47,23 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onBtnSobreAction() {
-		System.out.println("onMenuSobreAction");
+		loadView("/gui/Sobre.fxml");
+	}
+	
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			AnchorPane newPage = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			mainVBox.getChildren().set(1, newPage);
+		} catch (IOException e) {
+			Alerts.showAlert("Error", null, "Erro de carregamento de Página", AlertType.ERROR);
+			e.printStackTrace();
+		}
+		
 	}
 	
 	@Override
