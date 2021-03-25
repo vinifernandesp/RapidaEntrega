611cad388fc7d8771bf094a67b43295fcaa8d168
Vinícius Fernandes <vinifernandesp@outlook.com.br>
2021-03-25 17:49:08 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -36,40 +37,31 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onBtnCadastroAction() {
-		loadView("/gui/Cadastro.fxml");
+		loadView("/gui/Cadastro.fxml", x -> {});
 	}
 	
 	@FXML
 	public void onBtnBuscaAction() {
-		loadViewBusca("/gui/Busca.fxml");
+		loadView("/gui/Busca.fxml", (BuscaController controller) -> {
+			controller.setDeliveryService(new DeliveryService(new LocalizationService(), new ConsigneeService(), new SenderService()));
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
 	public void onBtnEdicaoAction() {
-		loadViewEdicaoExclusao("/gui/EdicaoExclusao.fxml");
+		loadView("/gui/EdicaoExclusao.fxml", (EdicaoExclusaoController controller) -> {
+			controller.setDeliveryService(new DeliveryService(new LocalizationService(), new ConsigneeService(), new SenderService()));
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
 	public void onBtnSobreAction() {
-		loadView("/gui/Sobre.fxml");
+		loadView("/gui/Sobre.fxml", x -> {});
 	}
 	
-	private synchronized void loadView(String absoluteName) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			AnchorPane newPage = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			mainVBox.getChildren().set(1, newPage);
-		} catch (IOException e) {
-			Alerts.showAlert("Error", null, "Erro de carregamento de Página", AlertType.ERROR);
-			e.printStackTrace();
-		}
-	}
-	
-	private void loadViewBusca(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			AnchorPane newPage = loader.load();
@@ -79,28 +71,8 @@ public class MainViewController implements Initializable {
 			
 			mainVBox.getChildren().set(1, newPage);
 			
-			BuscaController controller = loader.getController();
-			controller.setDeliveryService(new DeliveryService(new LocalizationService(), new ConsigneeService(), new SenderService()));
-			controller.updateTableView();
-		} catch (IOException e) {
-			Alerts.showAlert("Error", null, "Erro de carregamento de Página", AlertType.ERROR);
-			e.printStackTrace();
-		}
-	}
-	
-	private void loadViewEdicaoExclusao(String absoluteName) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			AnchorPane newPage = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			mainVBox.getChildren().set(1, newPage);
-			
-			EdicaoExclusaoController controller = loader.getController();
-			controller.setDeliveryService(new DeliveryService(new LocalizationService(), new ConsigneeService(), new SenderService()));
-			controller.updateTableView();
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 		} catch (IOException e) {
 			Alerts.showAlert("Error", null, "Erro de carregamento de Página", AlertType.ERROR);
 			e.printStackTrace();
